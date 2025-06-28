7a2567f1fade59155e46356872fde3234d5e0b0b
renan <renan.leallol@gmail.com>
2025-06-28 17:15:45 -0300

    feat: adiciona funcionalidade de cadastro de livros após scraping

--- src/infra/repositories/book_repository.py
+++ src/infra/repositories/book_repository.py
@@ -1,2 +1,14 @@
+from nest.core import Injectable
+from ..models.book_model import BookModel
+from ..db import SessionLocal
+
+
+@Injectable
 class BookRepository:
-    pass
+    def __init__(self):
+        pass
+
+    def create_many(self, books_data: list[BookModel]):
+        with SessionLocal() as session:
+            session.add_all(books_data)
+            session.commit()
