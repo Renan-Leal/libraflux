b9bbfa530c7f90d9226998ab1385c9e54e0a682c
rafaelfonsxca <rafael_010697@hotmail.com>
2025-07-09 16:39:59 -0300

    Fix: running lint.sh

--- src/domain/stats/stats_module.py
+++ src/domain/stats/stats_module.py
@@ -3,6 +3,7 @@ from .stats_service import StatsService
 from .stats_controller import StatsController
 from ...infra.repositories.book_repository_module import BookRepositoryModule
 
+
 @Module(
     imports=[BookRepositoryModule],
     providers=[StatsService],
