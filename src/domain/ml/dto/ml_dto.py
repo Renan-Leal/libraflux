78cde710d39b6b62532a61e3c909de1d76088d20
rafaelfonsxca <rafael_010697@hotmail.com>
2025-07-27 10:56:07 -0300

    Fix: adding docstring and common validators

--- src/domain/ml/dto/ml_dto.py
+++ src/domain/ml/dto/ml_dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from ....common.validators import BaseModel
 from typing import Optional
 
 
--- src/domain/ml/ml_controller.py
+++ src/domain/ml/ml_controller.py
@@ -16,12 +16,43 @@ class MlController:
 
     @Get("/features", description="Dados formatados para features.")
     def get_features(self) -> List[BookFeatureResponse]:
+        """Dados formatados para features.
+
+        Returns:
+            book_id: str
+            category: str
+            rating: int
+            price: float
+            availability: int
+            description_length: int
+        """
         return self.service.get_features()
 
     @Get("/training-data", description="Dataset para treinamento.")
     def get_training_data(self) -> List[TrainingDataResponse]:
+        """Dataset para treinamento.
+
+        Returns:
+            # features
+            category: str
+            price: float
+            availability: int
+            description_length: int
+            # target
+            rating: int
+        """
         return self.service.get_training_data()
 
     @Post("/predictions", description="Endpoint para receber predições.")
     def post_predictions(self, prediction: PredictionRequest) -> PredictionResponse:
+        """Endpoint para receber predições.
+
+        Args:
+            book_id: str
+            predicted_rating: float
+
+        Returns:
+            message: str
+            prediction_id: Optional[int] = None
+        """
         return self.service.save_prediction(prediction)
